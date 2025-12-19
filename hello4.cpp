0593043307e8df3c8a3852b547e28d657790b6e4
maaruthii1313-svg <maaruthii1313@gmail.com>
2025-12-19 09:52:14 +0530

    The fourth file is created

---
+++ hello4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a ,b;
+int sum;
+cout<<"Enter the values of a and b: ";
+cin>>a>>b;
+
+sum=a+b;
+
+cout<<"The sum is :"<<sum <<endl;
+return 0;
+
+}
